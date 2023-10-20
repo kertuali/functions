3a213c90be766bfc89a5cbd7aa580d8d65ae8ce5
Kertu Ali <kertu.greenberg@tktk.ee>
2023-10-20 16:58:27 +0200

    funnctions

---
+++ Functions/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Are you coming or going. (in/out)");
+
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{
+    Console.WriteLine("Hello, world!");
+}
+else
+{
+    Console.WriteLine("See you later aligator.");
+}
+
+PrintHello();
+PrintGoodBye();
+
+static void PrintHello()
+{
+    Console.WriteLine("Hello, world!");
+}
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later aligator.");
+}
